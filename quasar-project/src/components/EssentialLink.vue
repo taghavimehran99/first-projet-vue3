df98ad7acbd557bbe38b7c0bb9cff5d5e79fb8ac
Your Name <you@example.com>
2025-11-15 22:47:30 +0330

    add file and delete trash code defult in project

--- quasar-project/src/components/EssentialLink.vue
+++ quasar-project/src/components/EssentialLink.vue
@@ -1,36 +1,7 @@
 <template>
-  <q-item clickable tag="a" target="_blank" :href="props.link">
-    <q-item-section v-if="props.icon" avatar>
-      <q-icon :name="props.icon" />
-    </q-item-section>
-
-    <q-item-section>
-      <q-item-label>{{ props.title }}</q-item-label>
-      <q-item-label caption>{{ props.caption }}</q-item-label>
-    </q-item-section>
-  </q-item>
+<h1>slam</h1>
 </template>
 
 <script setup>
-const props = defineProps({
-  title: {
-    type: String,
-    required: true,
-  },
 
-  caption: {
-    type: String,
-    default: '',
-  },
-
-  link: {
-    type: String,
-    default: '#',
-  },
-
-  icon: {
-    type: String,
-    default: '',
-  },
-})
 </script>
