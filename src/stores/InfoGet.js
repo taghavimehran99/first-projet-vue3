77a515558bc01e2deef74f88b2791e89b24d96f3
Your Name <you@example.com>
2025-11-24 18:28:44 +0330

    delete trash  varebel in store

--- src/stores/InfoGet.js
+++ src/stores/InfoGet.js
@@ -7,11 +7,6 @@ export const useinfocomponentget = defineStore('infoget', {
       { id: 2, text: 'task explanation : ', plac: 'write task', value: '' },
     ],
     list: [],
-    InfoBtnRouters: [
-      { id: 1, text: 'Go To Route Home', function: '/', class: 'btn-router' },
-      { id: 2, text: 'Go To Route Detail', function: '/HomeDeatal', class: 'btn-router' },
-      { id: 3, text: 'Go To Route Get Api', function: '/GetApi', class: 'btn-router' },
-    ],
     InfoCard: [],
   }),
   getters: {
