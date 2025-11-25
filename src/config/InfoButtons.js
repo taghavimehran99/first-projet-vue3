d8d12dca3942b2181d4d7b2566690c8b76c536a7
Your Name <you@example.com>
2025-11-25 21:31:14 +0330

    delete test code in confige

--- src/config/InfoButtons.js
+++ src/config/InfoButtons.js
@@ -2,5 +2,4 @@ export const buttons=[
     { id: 1, text: 'Go To Route Home', path: '/', class: 'btn-router' },
     { id: 2, text: 'Go To Route Detail', path: '/HomeDeatal', class: 'btn-router' },
     { id: 3, text: 'Go To Route Get Api', path: '/GetApi', class: 'btn-router' },
-    { id: 3, text: 'test', path: '/GetApi', class: 'btn-router' },
 ]
